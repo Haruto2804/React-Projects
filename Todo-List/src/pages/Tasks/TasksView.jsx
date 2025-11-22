fabb1355df333bc3ec3f36bc6be371c0302a35db
= <=>
2025-11-22 20:36:28 +0700

    feat (TasksView): Use Location for  split URL path name and render tasks with Use Location

--- Todo-List/src/pages/Tasks/TasksView.jsx
+++ Todo-List/src/pages/Tasks/TasksView.jsx
@@ -1,11 +1,29 @@
-
+import { useLocation } from 'react-router-dom'
 export function TasksView({ todo }) {
+  const location = useLocation();
+  const currentView = location.pathname.split('/').pop();
+  const tasksToDisplay = todo || [];
+  let filteredTasks = [];
+  switch (currentView) {
+    case 'completed':
+      filteredTasks = tasksToDisplay.filter(task => task.completed === true);
+      break;
+    case 'upcoming':
+      filteredTasks = tasksToDisplay.filter(task => task.completed === false);
+      break;
+    default:
+      filteredTasks = tasksToDisplay;
+      break;
+
+  }
+  console.log(currentView);
   const currentTime = new Date();
   const dateString = currentTime.toLocaleString('default', {
     weekday: 'long',
     month: 'long',
     day: '2-digit'
   });
+
   return (
     <>
       <div className="ml-[258px] mr-80 flex-1 p-8 flex flex-col gap-5 overflow-y-auto">
@@ -15,14 +33,14 @@ export function TasksView({ todo }) {
             <p className="text-gray-500 text-lg">{dateString}</p>
           </div>
         </div>
-        {todo.map((task) => (
+        {filteredTasks.map((task) => (
           <div key={task.id} className="flex flex-col gap-2 w-full">
             <div className="flex items-center justify-between border-solid border
           border-gray-200 rounded-lg cursor-pointer hover:bg-slate-200 transition-all
           p-4">
-              <div className="flex gap-4 items-center">
-                <input type="checkbox" className="size-5 rounded cursor-pointer" />
-                <p className="">{task.todo}</p>
+              <div className="flex gap-4 items-center ">
+                <input disabled={task.completed} type="checkbox" className={` size-5 rounded cursor-pointer`} />
+                <p className={task.completed ? "line-through opacity-70" : ""}>{task.todo}</p>
               </div>
               <div className="flex gap-4 items-center">
                 <div className={
@@ -32,7 +50,7 @@ export function TasksView({ todo }) {
                 ${task.priority === 'High' ? "text-red-500 bg-red-100"
                     : task.priority === 'Medium' ? "text-yellow-700 bg-yellow-100"
                       : task.priority === "Low" ? "text-gray-700 bg-gray-200"
-                        :  "text-green-700 bg-green-200" 
+                        : "text-green-700 bg-green-200"
                   }
                 font-semibold 
                 rounded-full 
